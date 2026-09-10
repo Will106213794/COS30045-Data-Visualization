192e9637a101e9af640ca9f1f898a15ba9072a8f
Will106213794 <106213794@student.swin.edu.au>
2026-09-10 19:08:10 +0800

    commit

--- Exercise 4/Exercise 4.5/energy-webpage-v2/js/main.js
+++ Exercise 4/Exercise 4.5/energy-webpage-v2/js/main.js
@@ -32,5 +32,35 @@ d3.csv("data/tvBrandCount.csv", d => {
     console.log(d3.extent(data, d => d.count));
 data.sort((a, b) => b.count - a.count);
 
-drawBarChart(data); // Call Function which will draw in the next exercise
+drawBarChart(data); 
 });
+
+
+const drawBarChart = data => {
+
+    // Code for bar chart goes here
+    svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+
+    // make the bars proportional to the count value
+    .attr("width", d => d.count) // as long as the count is not too big, we can multiply it by 10 to make it more visible
+    .attr("height", barHeight)
+    .attr("fill", "steelblue")
+
+    // Space out the bars
+    .attr("x", 0)
+    .attr("y", (d, i) => i * (barHeight + barSpacing))
+
+    // Add a class to each bar based on the count value
+    .attr("class", d => {
+    console.log(d);
+     return `bar bar-${d.count}`;
+    });
+
+  
+  };
+
+const barHeight = 20;
+const barSpacing = 5;
